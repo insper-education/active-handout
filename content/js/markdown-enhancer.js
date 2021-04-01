58187110a50419a6d8c0ea22bbf49247ac1aa03a
Igor Montagner <igordsm@gmail.com>
2021-04-01 17:28:41 -0300

    Better detection for config.yml

--- content/js/markdown-enhancer.js
+++ content/js/markdown-enhancer.js
@@ -1,7 +1,7 @@
 {
-    if (! window.site_url) {
-        window.site_url = "http://127.0.0.1:8000/";
-    }
+    // HACK: mkdocs-material specific
+    let home_link = document.querySelector(".md-header a.md-header-nav__button.md-logo");
+    window.site_url = home_link.href;
     console.log(window.site_url + "config.yml");
     fetch(window.site_url + "config.yml").then((resp) => {
         return resp.text();
