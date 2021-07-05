f56e3f578356e399f0ab36518da3823309784885
Igor Montagner <igordsm@gmail.com>
2021-07-05 17:02:36 -0300

    Progress shows notification if user has not answered all questions. fix bug in counter.

--- content/js/counter.js
+++ content/js/counter.js
@@ -1,5 +1,8 @@
 {
-    let admonitions_for_counting = ["question", "example"];
+    let admonitions_for_counting = ["question"];
+    if (window.ihandout_config["counter"]) {
+        admonitions_for_counting = window.ihandout_config["counter"];
+    }
 
     for (const admonition_type of admonitions_for_counting) {
         let elements = document.querySelectorAll(".admonition." + admonition_type);
