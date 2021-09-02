47290a018dd54125ce33a53fe2fe38ee10975217
Igor Montagner <igordsm@gmail.com>
2021-09-02 16:15:45 -0300

    Improvements in styling of modal images.

--- content/js/modal-images.js
+++ content/js/modal-images.js
@@ -2,83 +2,51 @@
     // Rafael Corsi @ insper . edu . br
     // maio 2021
     //
-    // based on: https://jsfiddle.net/snowMonkey/f1zav0ge/
 
-    let style_progress = document.head.appendChild(document.createElement("style"));
+    const style_progress = document.head.appendChild(document.createElement('style'));
     style_progress.innerText = `
-    .modal {
-    display: none;
-    position: fixed;
-    padding-top: 100px;
-    width: 100%;
-    height: 100%;
-    overflow: auto;
-    background-color: rgba(0, 0, 0, 0.6);
-    }
-
-    .modal-content {
-    margin: auto;
-    display: block;
-    width: 90%;
-    }
-
-    .close {
-    position: relative;
-    top: -40px;
-    right: -40px;
-    color: #000;
-    font-size: 40px;
-    transition: 0.3s;
-    }
-
-    .close:hover,
-    .close:focus {
-    color: #FFF;
-    text-decoration: none;
-    cursor: pointer;
-    transition: 0.5s;
-    }
-
-    @media only screen and (max-width: 700px) {
-    .modal-content {
+    #modalContainer {
+        position: fixed;
+        top: 0px;
+        left: 0px;
         width: 100%;
+        height: 100%;
+        z-index: 200;
+        
+        display: none;
+        background-color: rgba(0, 0, 0, 0.6);
+
+        padding: 50px;
     }
-    }`;
 
-    let mDiv = document.createElement('div');
-    mDiv.className = "modal";
-    mDiv.id = "modalImg";
+    #modalContainer img {
+        margin: auto;
+        max-width: 100%;
+        max-height: 100%;
+        width: auto;
+        height: auto;
 
-    let mSpan = document.createElement('span')
-    mSpan.className = "close";
-    mSpan.innerHTML = "&times";
+        display: block;
+    }
+    `;
 
-    let mImg = document.createElement('img');
-    mImg.className = "modal-content";
-    mImg.id = "imgNone";
+    const modalContainer = document.createElement('div');
+    modalContainer.className = 'modal';
+    modalContainer.id = 'modalContainer';
+    modalContainer.innerHTML = '<img>';
 
-    mDiv.appendChild(mSpan);
-    mDiv.appendChild(mImg);
+    const modalImg = modalContainer.querySelector('img');
+    modalContainer.addEventListener('click', function () {
+        modalContainer.style.display = 'none';
+    });
 
-    main = document.getElementsByClassName("md-container")[0];
-    main.appendChild(mDiv);
+    document.body.appendChild(modalContainer);
 
-    let images = document.images;
+    const images = document.images;
     for (let i = 0; i < images.length; i++) {
-        images[i].addEventListener("click", function() {
-            mDiv.style.display = "block";
-            mImg.src = this.src;
+        images[i].addEventListener('click', function () {
+            modalContainer.style.display = 'block';
+            modalImg.src = this.src;
         });
     }
-
-    let span = document.querySelector(".close");
-    span.addEventListener("click", function() {
-        mDiv.style.display = "none";
-    });
-
-    document.addEventListener("keydown", (evt) => {
-        if (evt.keyCode == 27 || window.event) {
-            mDiv.style.display = "none";
-        }
-    });
 }
