95e85375886162e3aabb11c06b4682fae60f5958
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2022-09-21 20:04:06 -0300

    Starting redesign tests

---
+++ content/examples/python-intro/hello.py
@@ -0,0 +1,8 @@
+"""
+Programa que escreve "Hello, world!" no console.
+Serve para verificar se conseguimos usar o Python pela primeira vez.
+
+@author: Profs. Developer Life
+"""
+
+print('Hello, world!')
