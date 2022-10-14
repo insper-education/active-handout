df6740a961024760e9a3a98aed76872915ff6813
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2022-10-14 16:29:38 -0300

    Remove example content

--- content/examples/python-intro/hello.py
+++
@@ -1,8 +0,0 @@
-"""
-Programa que escreve "Hello, world!" no console.
-Serve para verificar se conseguimos usar o Python pela primeira vez.
-
-@author: Profs. Developer Life
-"""
-
-print('Hello, world!')
