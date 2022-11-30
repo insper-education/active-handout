825c79e7a55c48d1b2730663f2a8c1e73f6ee4c9
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2022-11-30 15:04:27 -0300

    Exercise hook example

---
+++ hooks/custom_exercise.py
@@ -0,0 +1,23 @@
+import random
+from active_handout_plugins import register_exercise_visitor_builder, ExerciseAdmonition
+
+
+def on_startup(*args, **kwargs):
+    register_exercise_visitor_builder(RandomQuestionExercise, 4)
+
+
+class RandomQuestionExercise(ExerciseAdmonition):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__('exercise', ['random-question'], *args, **kwargs)
+
+    def create_exercise_form(self, el, submission_form):
+        questions = ["What's your name?", "What is the answer to life, the universe and everything?"]
+
+        return f'''
+        <p>{random.choice(questions)}</p>
+        <input type="text" value="" name="data"/>
+        <input class="ah-button ah-button--primary" type="submit" value="Submit"/>
+        '''
+
+    def get_tags(self, el):
+        return ['random-question']
